c6748a0486ab06acf757d5edc19399b5639ab016
kodkid <math_moss@hotmail.com>
2024-12-09 23:38:46 -0400

    Eu quero dormir

--- raizes_da_arte/views.py
+++ raizes_da_arte/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+TEMPLATES = [
+    {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': 'raizes_da_arte/template',
+    }
+]
+
 # Create your views here.
 def raizes_da_arte(request):
     return render(request, 'index.html')
