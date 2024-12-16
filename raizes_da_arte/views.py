0f57b677800ee13bc28c6acf50167b3112eda893
kodkid <math_moss@hotmail.com>
2024-12-15 20:49:54 -0400

    Tentando configurar as views e models previa para a configuraçao no migrations

--- raizes_da_arte/views.py
+++ raizes_da_arte/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+# from django.contrib.auth.hashers import make_password 'importando codigo para converter senha em hash'
+from .models import User
 TEMPLATES = [
     {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -15,6 +16,23 @@ def login(request):
     return render(request, 'login.html')
 
 def cadastro(request):
+    # Captando os dados do usuario da pagina cadastro
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+       # senha_hash = make_password(senha) 'Codigo para converter senha em hash'
+        #Transformando os dados do usuario em um objeto chamado user
+        user = User.objects.create(  
+            nome=nome,
+            email=email,
+            senha=senha
+        )
+
+        
+        return redirect('sucesso')
+
     return render(request, 'cadastro.html')
 
 def perfil(request):
@@ -28,3 +46,7 @@ def perfis(request):
 
 def eventos(request):
     return render(request, 'eventos.html')
+
+
+
+    
